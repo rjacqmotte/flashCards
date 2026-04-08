48534b1e3341c1fe78f463021f7d307dcd9f5e7b
rjacqmotte <rjacqmotte@gmail.com>
2026-04-08 11:35:53 +0200

    create variables.css global sheet

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
+import './styles/variables.css';
 import './index.css';
 import App from './app/App';
 import { Provider } from "react-redux";
